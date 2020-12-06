edfdb7c84fd29cede841c3d25369a158cca8ee6b
Rodrigo Avarez <ogirdorsystems@gmail.com>
2020-12-06 17:51:39 +1300

    Working on the Menu component and icons.

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
 const MenuComponent: React.FunctionComponent = () => {
+  const menu = [
+    {
+      id: 1,
+      title: 'Menu A'
+    }, {
+      id: 2,
+      title: 'Menu B'
+    }, {
+      id: 3,
+      title: 'Menu C'
+    }
+  ];
+
   return (
-    <h1>Menu</h1>
+    <div className="menu-component">
+      <ul className="menu-list">
+        {menu.map(item => (
+          <li key={item.id} className="menu-item">{item.title}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
